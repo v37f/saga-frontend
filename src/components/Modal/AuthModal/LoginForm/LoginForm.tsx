bb5704dadd000b045b4d6e2979993dfcca01b5fd
Vladimir Frolov <frolov_vo@inbox.ru>
2024-06-25 19:08:38 +0300

    fear: add login form validation

--- src/components/Modal/AuthModal/LoginForm/LoginForm.tsx
+++ src/components/Modal/AuthModal/LoginForm/LoginForm.tsx
@@ -1,27 +1,113 @@
 import InputTypeText from 'src/ui/inputs/InputTypeText/InputTypeText';
 import styles from './LoginForm.module.scss';
 import SolidButton from 'src/ui/buttons/SolidButton/SolidButton';
+import * as yup from 'yup';
+import { PASSWORD_REGEX } from 'src/utils/constants';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const loginSchema = yup.object({
+  email: yup
+    .string()
+    .required('Введите email')
+    .email('Введен некорректный email адрес'),
+  password: yup
+    .string()
+    .required('Введите пароль')
+    .matches(PASSWORD_REGEX, 'Пароль содержать только латниские буквы и цыфры')
+    .min(8, 'Пароль должен содержать минимум 8 символов'),
+  isSeller: yup.boolean().required(),
+});
+
+type LoginInputs = {
+  email: string;
+  password: string;
+  isSeller: boolean;
+};
 
 const LoginForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginInputs>({
+    mode: 'all',
+    resolver: yupResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      isSeller: false,
+    },
+  });
   const requestError = '';
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    console.log({
+      email: data.email,
+      password: data.password,
+      userRole: data.isSeller ? 'seller' : 'customer',
+    });
+  };
+
   return (
     <form
       className={styles.loginForm}
-      onSubmit={() => console.log('submit')}
+      onSubmit={handleSubmit(onSubmit)}
       noValidate
     >
       <fieldset className={styles.inputs}>
-        <InputTypeText label="Email" type="text" error="" />
-        <InputTypeText label="Пароль" type="password" error="" />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputTypeText
+              label="Email"
+              type="text"
+              error={errors.email?.message}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+            />
+          )}
+          name="email"
+        />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputTypeText
+              label="Пароль"
+              type="password"
+              error={errors.password?.message}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+            />
+          )}
+          name="password"
+        />
         <div className={styles.checkbox}>
-          <input type="checkbox" className={styles.checkboxInput} id="seller" />
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <input
+                type="checkbox"
+                className={styles.checkboxInput}
+                id="seller"
+                onBlur={onBlur}
+                onChange={onChange}
+                checked={value}
+              />
+            )}
+            name="isSeller"
+          />
           <label htmlFor="seller" className={styles.checkboxLabel}>
             Войти как продавец
           </label>
         </div>
       </fieldset>
       <p className={styles.requestError}>{requestError}</p>
-      <SolidButton type="submit">Войти</SolidButton>
+      <SolidButton type="submit" disabled={!isValid}>
+        Войти
+      </SolidButton>
     </form>
   );
 };
