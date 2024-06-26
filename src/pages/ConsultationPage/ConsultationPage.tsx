7cfc94272fe21155e5acc862ca05767541000672
Vladimir Frolov <frolov_vo@inbox.ru>
2024-06-27 01:40:15 +0300

    feat: create title and description for consultaiotn page

--- src/pages/ConsultationPage/ConsultationPage.tsx
+++ src/pages/ConsultationPage/ConsultationPage.tsx
@@ -1,5 +1,16 @@
+import styles from './ConsultationPage.module.scss';
+
 const ConsultationPage = () => {
-  return <main>Арт-консультация</main>;
+  return (
+    <main className={styles.consultationPage}>
+      <h2 className={styles.title}>Арт-консультация</h2>
+      <p className={styles.description}>
+        Цена арт-объекта определяется на основе результатов работы авторского
+        алгоритма для анализа больших данных, использующий машинное обучение, и
+        учитывающий 35 критериев оценки арт-объекта и его автора.
+      </p>
+    </main>
+  );
 };
 
 export default ConsultationPage;
