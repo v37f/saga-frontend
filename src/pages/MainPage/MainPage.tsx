174ff0349d57f7a7677d70e98c2f2c25e404af0a
Vladimir Frolov <frolov_vo@inbox.ru>
2024-06-26 13:17:26 +0300

    feat: add vercel.json

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import styles from './MainPage.module.scss';
 
 const MainPage = () => {
-  return <main className={styles.mainPage}></main>;
+  return <main className={styles.mainPage}>Главная</main>;
 };
 
 export default MainPage;
