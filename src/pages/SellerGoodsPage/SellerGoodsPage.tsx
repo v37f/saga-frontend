7f3d9c3adc9e20b633cd03f5097b591f0fb8971b
Vladimir Frolov <frolov_vo@inbox.ru>
2024-07-02 00:09:13 +0300

    fix: some minor fixes

--- src/pages/SellerGoodsPage/SellerGoodsPage.tsx
+++ src/pages/SellerGoodsPage/SellerGoodsPage.tsx
@@ -1,10 +1,7 @@
 import styles from './SellerGoodsPage.module.scss';
 import SolidButton from 'src/ui/buttons/SolidButton/SolidButton';
-import { NOT_FOUND_ROUTE } from 'src/utils/constants';
-import { useNavigate } from 'react-router-dom';
 
 const SellerGoodsPage = () => {
-  const navigate = useNavigate();
   return (
     <main className={styles.sellerGoodsPage}>
       <div className={styles.sellerGoodsPage__container}>
@@ -12,10 +9,10 @@ const SellerGoodsPage = () => {
         <div className={styles.sellerGoodsPage__content}>
           <p className={styles.sellerGoodsPage__empty}>Пока здесь пусто</p>
           <p className={styles.sellerGoodsPage__text}>
-            Начните добавлять товары на продажу. Они появятся на этой странице
+            Начните добавлять товары на продажу. Они появятся на этой странице
           </p>
           <div className={styles.sellerGoodsPage__button}>
-            <SolidButton onClick={() => navigate(NOT_FOUND_ROUTE)}>
+            <SolidButton>
               <span className={styles.sellerGoodsPage__buttonImg}></span>
               Добавить товар
             </SolidButton>
